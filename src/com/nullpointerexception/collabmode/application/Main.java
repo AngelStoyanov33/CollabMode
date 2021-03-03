db79db92786fc2da434f58bce203090490100524
Angel Stoyanov <aststoyanov@gmail.com>
2021-03-03 17:31:35 +0200

    Refactored main and changed register.fxml layout from GridPane to BorderPane

--- src/com/nullpointerexception/collabmode/application/Main.java
+++ src/com/nullpointerexception/collabmode/application/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/register.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Register.fxml"));
+        BorderPane root = (BorderPane) loader.load();
+        Scene scene = new Scene(root,900,725);
+        scene.getStylesheets().add(getClass().getResource("../resources/css/application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("CollabMode v0.0.1-SNAPSHOT");
+
         primaryStage.show();
     }
 
