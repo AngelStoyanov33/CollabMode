da11ba7a1d820156f60a19e454ac30442ccf1e28
AngelStoyanov33 <aststoyanov@gmail.com>
2021-03-08 21:06:04 +0200

    Added a user model

---
+++ src/main/java/com/nullpointerexception/collabmode/model/User.java
@@ -0,0 +1,37 @@
+package com.nullpointerexception.collabmode.model;
+
+public class User {
+    private int id;
+    private String fullName;
+    private String email;
+
+    public User(int id, String fullName, String email) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
