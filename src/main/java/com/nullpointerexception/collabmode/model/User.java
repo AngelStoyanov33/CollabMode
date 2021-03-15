81ed9e20b55fd422a654f22b77f8a492a9b3614b
AngelStoyanov33 <aststoyanov@gmail.com>
2021-03-15 18:43:39 +0200

    Updated User model to store team information

--- src/main/java/com/nullpointerexception/collabmode/model/User.java
+++ src/main/java/com/nullpointerexception/collabmode/model/User.java
@@ -4,11 +4,15 @@ public class User {
     private int id;
     private String fullName;
     private String email;
+    private int teamID;
+    private boolean isTeamOwner;
 
-    public User(int id, String fullName, String email) {
+    public User(int id, String fullName, String email, int teamID, boolean isTeamOwner) {
         this.id = id;
         this.fullName = fullName;
         this.email = email;
+        this.teamID = teamID;
+        this.isTeamOwner = isTeamOwner;
     }
 
     public int getId() {
@@ -33,5 +37,21 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    public boolean isTeamOwner() {
+        return isTeamOwner;
+    }
+
+    public void setTeamOwner(boolean teamOwner) {
+        isTeamOwner = teamOwner;
     }
 }
