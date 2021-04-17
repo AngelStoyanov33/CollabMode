9f0ff31c70d915ff69b034a074e4ebfcd14f365d
AngelStoyanov33 <aststoyanov@gmail.com>
2021-04-17 14:38:45 +0300

    Included RichTextFX and replaced TextArea with CodeArea

--- src/main/java/com/nullpointerexception/collabmode/controller/DashboardController.java
+++ src/main/java/com/nullpointerexception/collabmode/controller/DashboardController.java
@@ -1,6 +1,5 @@
 package com.nullpointerexception.collabmode.controller;
 
-
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTreeView;
 import com.nullpointerexception.collabmode.application.Main;
@@ -9,22 +8,70 @@ import com.nullpointerexception.collabmode.service.FTPManager;
 import com.nullpointerexception.collabmode.service.HTTPRequestManager;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.util.Duration;
 import org.apache.commons.net.ftp.FTPFile;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.GenericStyledArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.Paragraph;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.reactfx.Subscription;
+import org.reactfx.collection.ListModification;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DashboardController {
+
+    private static final String[] KEYWORDS = new String[] {
+            "abstract", "assert", "boolean", "break", "byte",
+            "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else",
+            "enum", "extends", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import",
+            "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws",
+            "transient", "try", "void", "volatile", "while"
+    };
+
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    private static final String PAREN_PATTERN = "\\(|\\)";
+    private static final String BRACE_PATTERN = "\\{|\\}";
+    private static final String BRACKET_PATTERN = "\\[|\\]";
+    private static final String SEMICOLON_PATTERN = "\\;";
+    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
+
+    private static final Pattern PATTERN = Pattern.compile(
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
+                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+                    + "|(?<STRING>" + STRING_PATTERN + ")"
+                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+
+    private ExecutorService executor;
 
     private final static int INTERVAL = 2 * 60; //2 minutes (120 seconds)
     private static String token = "";
@@ -33,7 +80,7 @@ public class DashboardController {
 
     @FXML private JFXTreeView<String> treeView;
     @FXML private MenuBar menuBar;
-    @FXML private JFXTextArea textArea;
+    @FXML private CodeArea codeArea;
 
     @FXML private Menu collaborateMenu;
     @FXML private MenuItem addTeam;
@@ -131,6 +178,53 @@ public class DashboardController {
             });
             myTeamMenu.getItems().add(transferOwnerItem);
         }
+
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+        codeArea.setContextMenu( new DefaultContextMenu() );
+        codeArea.getVisibleParagraphs().addModificationObserver
+                (
+                        new VisibleParagraphStyler<>( codeArea, this::computeHighlighting )
+                );
+
+        // auto-indent: insert previous line's indents on enter
+        final Pattern whiteSpace = Pattern.compile( "^\\s+" );
+        codeArea.addEventHandler( KeyEvent.KEY_PRESSED, KE ->
+        {
+            if ( KE.getCode() == KeyCode.ENTER ) {
+                int caretPosition = codeArea.getCaretPosition();
+                int currentParagraph = codeArea.getCurrentParagraph();
+                Matcher m0 = whiteSpace.matcher( codeArea.getParagraph( currentParagraph-1 ).getSegments().get( 0 ) );
+                if ( m0.find() ) Platform.runLater( () -> codeArea.insertText( caretPosition, m0.group() ) );
+            }
+        });
+
+
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), ev -> {
+            codeArea.replaceText(0, 0, codeArea.getText());
+        }));
+        timeline.play();
+
+
+
+//        executor = Executors.newSingleThreadExecutor();
+//        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+//        Subscription cleanupWhenDone = codeArea.multiPlainChanges()
+//                .successionEnds(java.time.Duration.ofMillis(500))
+//                .supplyTask(this::computeHighlightingAsync)
+//                .awaitLatest(codeArea.multiPlainChanges())
+//                .filterMap(t -> {
+//                    if(t.isSuccess()) {
+//                        return Optional.of(t.get());
+//                    } else {
+//                        t.getFailure().printStackTrace();
+//                        return Optional.empty();
+//                    }
+//                })
+//                .subscribe(this::applyHighlighting);
+
+
+
 
         createProjectItem.setOnAction(event -> {
             TextInputDialog textInputDialog = new TextInputDialog();
@@ -363,7 +457,7 @@ public class DashboardController {
                     token = "";
                     currentUser = null;
                     Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Registration error");
+                    alert.setTitle("Session error");
                     alert.setContentText(json.get("errorMessage").toString());
                     alert.showAndWait();
                     Main.openLoginStage();
@@ -417,4 +511,97 @@ public class DashboardController {
             }
         }
     }
+
+    private StyleSpans<Collection<String>> computeHighlighting(String text) {
+        Matcher matcher = PATTERN.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder
+                = new StyleSpansBuilder<>();
+        while(matcher.find()) {
+            String styleClass =
+                    matcher.group("KEYWORD") != null ? "keyword" :
+                            matcher.group("PAREN") != null ? "paren" :
+                                    matcher.group("BRACE") != null ? "brace" :
+                                            matcher.group("BRACKET") != null ? "bracket" :
+                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
+                                                            matcher.group("STRING") != null ? "string" :
+                                                                    matcher.group("COMMENT") != null ? "comment" :
+                                                                            null; /* never happens */ assert styleClass != null;
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+    }
+
+    private class VisibleParagraphStyler<PS, SEG, S> implements Consumer<ListModification<? extends Paragraph<PS, SEG, S>>>
+    {
+        private final GenericStyledArea<PS, SEG, S> area;
+        private final Function<String,StyleSpans<S>> computeStyles;
+        private int prevParagraph, prevTextLength;
+
+        public VisibleParagraphStyler( GenericStyledArea<PS, SEG, S> area, Function<String,StyleSpans<S>> computeStyles )
+        {
+            this.computeStyles = computeStyles;
+            this.area = area;
+        }
+
+        @Override
+        public void accept( ListModification<? extends Paragraph<PS, SEG, S>> lm )
+        {
+            if ( lm.getAddedSize() > 0 )
+            {
+                int paragraph = Math.min( area.firstVisibleParToAllParIndex() + lm.getFrom(), area.getParagraphs().size()-1 );
+                String text = area.getText( paragraph, 0, paragraph, area.getParagraphLength( paragraph ) );
+
+                if ( paragraph != prevParagraph || text.length() != prevTextLength )
+                {
+                    int startPos = area.getAbsolutePosition( paragraph, 0 );
+                    Platform.runLater( () -> area.setStyleSpans( startPos, computeStyles.apply( text ) ) );
+                    prevTextLength = text.length();
+                    prevParagraph = paragraph;
+                }
+            }
+        }
+    }
+
+    private class DefaultContextMenu extends ContextMenu
+    {
+        private MenuItem fold, unfold, print;
+
+        public DefaultContextMenu()
+        {
+            fold = new MenuItem( "Fold selected text" );
+            fold.setOnAction( AE -> { hide(); fold(); } );
+
+            unfold = new MenuItem( "Unfold from cursor" );
+            unfold.setOnAction( AE -> { hide(); unfold(); } );
+
+            print = new MenuItem( "Print" );
+            print.setOnAction( AE -> { hide(); print(); } );
+
+            getItems().addAll( fold, unfold, print );
+        }
+
+        /**
+         * Folds multiple lines of selected text, only showing the first line and hiding the rest.
+         */
+        private void fold() {
+            ((CodeArea) getOwnerNode()).foldSelectedParagraphs();
+        }
+
+        /**
+         * Unfold the CURRENT line/paragraph if it has a fold.
+         */
+        private void unfold() {
+            CodeArea area = (CodeArea) getOwnerNode();
+            area.unfoldParagraphs( area.getCurrentParagraph() );
+        }
+
+        private void print() {
+            System.out.println( ((CodeArea) getOwnerNode()).getText() );
+        }
+    }
 }
+
